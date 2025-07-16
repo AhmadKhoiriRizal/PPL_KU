003e3e565bb00ae57bc852914022c107e9bcc4eb
AhmadKhoiriRizal <rizalice14@gmail.com>
2025-07-16 14:32:45 +0700

    First

---
+++ resources/views/kosong.blade.php
@@ -0,0 +1,18 @@
+<div class="container">
+                <div class="wrapper row2">
+                    <section class="hoc container clear">
+                        <article class="one_third first">
+                            <h6 class="my-3">Sejarah Saka Bhayangkara</h6>
+                            <p>Sejarah berdirinya Saka Bhayangkara diawali dengan dikeluarkannya instruksi bersama Menteri/Panglima Polisi dan Kwartir Nasional nomor Pol. 28/Inst./MK/1966 dan SK Kwarnas Nomor 4/1966 tertanggal 1 Juli 1966. Instruksi bersama tersebut berisi tentang pembentukan Pramuka Kamtibmas (Keamanan dan Ketertiban Masyarakat).</p>
+                            <p class="text-muted">Pramuka Kamtibmas sendiri memiliki sembilan krida, yaitu Krida Lalu Lintas, Krida SAR, Krida Pemadam Kebakaran, Krida Tindakan Pertama pada Kejadian Perkara, Krida Pengawal, Krida Komlek, Krida Pelacak, dan Krida Pengamat.</p>
+                            <p><a class="btn" href="#">Info Lebih lanjut <i class="fas fa-angle-right"></i></a></p>
+                        </article>
+                        <figure class="one_third">
+                            <a class="imgover" href="#"><img src="images/demo/gallery/Logo Saka.png" alt=""></a>
+                        </figure>
+                        <figure class="one_third">
+                            <a class="imgover" href="#"><img src="images/demo/gallery/4Krida-SakaBhayangkara.png" alt=""></a>
+                        </figure>
+                    </section>
+                </div>
+            </div>
